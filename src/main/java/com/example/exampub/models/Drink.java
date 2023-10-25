5642a74c56ecc4bd131ed7cf1044bfdf33a3fd56
Slithercze <Slithercze@gmail.com>
2023-10-26 01:24:12 +0200

    add model drink

---
+++ src/main/java/com/example/exampub/models/Drink.java
@@ -0,0 +1,20 @@
+package com.example.exampub.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "drinks")
+public class Drink {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private double price;
+    private boolean isForAdult;
+
+}
