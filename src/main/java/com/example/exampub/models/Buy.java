40d8b71d13eb5a7dcb21539e343c071920177956
Slithercze <Slithercze@gmail.com>
2023-10-26 01:37:22 +0200

    add buy model

---
+++ src/main/java/com/example/exampub/models/Buy.java
@@ -0,0 +1,12 @@
+package com.example.exampub.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Buy {
+    private Long userId;
+    private Long drinkId;
+    private boolean price;
+}
